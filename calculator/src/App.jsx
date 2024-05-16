743bf35dc87fd3b9f5dd1814794dfed3431f122f
Fabiola-cc <fcontrerascol@gmail.com>
2024-05-16 15:15:34 -0600

    feat: button handlers

--- calculator/src/App.jsx
+++ calculator/src/App.jsx
@@ -4,12 +4,44 @@ import ButtonSection from './components/Buttons'
 import View from './components/view'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [display, setDisplay] = useState('');
+  const [result, setResult] = useState(false)
+  const [calculate, setCalculate] = useState('');
+
+  const handleEqualClick = () => {
+      setResult(true)
+      try {
+          setDisplay(eval(calculate)); // eval is used here for simplicity, be cautious using it in production
+      } catch (error) {
+          setResult('Error');
+      }
+  };
+
+  const handleNumberClick = (number) => {
+      let negative = ''
+      if (number === '⁺/₋') {
+          if (actualDisplay[0] !== '-') {
+              negative = '-'
+          }
+          number = ''
+      }
+      setDisplay(prevDisplay => negative + prevDisplay + number)
+    };
+  
+    const handleOperatorClick = (operator) => {
+      if (operator === '=') {
+          handleEqualClick()
+      } else {
+          setDisplay('');
+          setCalculate(actualDisplay + ' ' + operator + ' ');
+      }
+    };
+
 
   return (
     <>
-      <View value='123456789000' result={true} />
-      <ButtonSection/>
+      <View value= {display} result={result} />
+      <ButtonSection handleNumberClick={handleNumberClick} handleOperatorClick={handleOperatorClick}/>
     </>
   )
 }
--- calculator/src/components/Buttons.jsx
+++ calculator/src/components/Buttons.jsx
@@ -1,15 +1,20 @@
-import React from 'react';
+import PropTypes from 'prop-types';
 import './Buttons.css'
 import Operands from './operands'
 import Operators from './operators'
 
-const ButtonSection = () => {
-  return (
-    <div className="buttons">
-      <Operands/>
-      <Operators/>
-    </div>
-  );
+const ButtonSection = ({handleNumberClick}, {handleOperatorClick}) => {
+    return (
+        <div className="buttons">
+        <Operands onClick={handleNumberClick}/>
+        <Operators onClick={handleOperatorClick}/>
+        </div>
+    );
+};
+
+ButtonSection.propTypes = {
+    handleNumberClick: PropTypes.func.isRequired,
+    handleOperatorClick: PropTypes.func.isRequired,
 };
 
 export default ButtonSection;
